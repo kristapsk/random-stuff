3ef4e4efdf3a9615672dfaf88419d0d241294b5c
Kristaps Kaupe <kristaps.k@intelligentsystems.lv>
2014-08-28 20:16:22 +0300

    Script to create local Gentoo portage overlay

---
+++ create_local_overlay.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+if [ ! -f /etc/gentoo-release ]; then
+    echo "Must be Gentoo"
+    exit
+fi
+
+if [ "`id -u`" != "0" ]; then
+    echo "Must be root!"
+    exit
+fi
+
+if [ -d /usr/local/portage ]; then
+    echo "/usr/local/portage already exists!"
+    exit
+fi
+
+mkdir -p /usr/local/portage/{metadata,profiles}
+echo "`hostname`" > /usr/local/portage/profiles/repo_name
+echo "masters = gentoo" > /usr/local/portage/metadata/layout.conf
+chown -R portage:portage /usr/local/portage
+
+echo "PORTDIR_OVERLAY=\"/usr/local/portage ${PORTDIR_OVERLAY}\"" >> /etc/portage/make.conf
+
