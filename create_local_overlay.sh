d4a24f9787724dd120c682eb99b33978c6b2bb54
Kristaps Kaupe <kristaps.k@intelligentsystems.lv>
2015-01-19 14:33:15 +0200

    Better permissions and fix PORTDIR_OVERLAY+layman.

--- create_local_overlay.sh
+++ create_local_overlay.sh
@@ -19,6 +19,8 @@ mkdir -p /usr/local/portage/{metadata,profiles}
 hostname > /usr/local/portage/profiles/repo_name
 echo "masters = gentoo" > /usr/local/portage/metadata/layout.conf
 chown -R portage:portage /usr/local/portage
+# Allow non-root portage group users to put ebuilds to local overlay
+chmod g+w /usr/local/portage
 
-echo "PORTDIR_OVERLAY=\"/usr/local/portage ${PORTDIR_OVERLAY}\"" >> /etc/portage/make.conf
+echo "PORTDIR_OVERLAY=\"/usr/local/portage \${PORTDIR_OVERLAY}\"" >> /etc/portage/make.conf
 
